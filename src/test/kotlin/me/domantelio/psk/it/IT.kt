27b5aa759fd5195c676813da37609283e27d5c27
Domantelio✨ <domantelio@proton.me>
2024-05-12 23:25:49 +0300

    feat(it-tests): split tests into more simple use-cases

--- src/test/kotlin/me/domantelio/psk/it/IT.kt
+++ src/test/kotlin/me/domantelio/psk/it/IT.kt
@@ -45,6 +45,71 @@ class IT {
     private val logger = LoggerFactory.getLogger(IT::class.java)
 
     @Test
+    fun createInvoiceAddItem() {
+        val INVOICE_NAME  = "test-invoice-${rng.nextLong()}"
+        val ITEM_NAME = "test-item-${rng.nextLong()}"
+        val ITEM_PRICE = rng.nextInt(1000).toString()
+        val ITEM_DESC = "test-description-${rng.nextLong()}"
+
+        open("/index.xhtml")
+
+        s("[id=\"invoiceForm:name\"]").setValue(INVOICE_NAME)
+        s("[id=\"invoiceForm:submitInvoice\"]").click()
+
+        s("[id=\"j_id_q:itemName\"]").setValue(ITEM_NAME)
+        s("[id=\"j_id_q:itemPrice\"]").setValue(ITEM_PRICE)
+        s("[id=\"j_id_q:itemDescription\"]").setValue(ITEM_DESC)
+        s("[id=\"j_id_q:j_id_x\"]").click()
+
+        // Go to index
+        s("body > a").click()
+
+        var invoiceTable = s("#invoice-table")
+        logger.debug("invoiceTable = ${invoiceTable.text}")
+        assertThat(invoiceTable.text).contains(INVOICE_NAME)
+
+        // Go to OTHERS
+        s("body > a").click()
+
+        val itemTable = s("#item-table")
+        logger.debug("itemTable = ${itemTable.text}")
+        assertThat(itemTable.text).contains(ITEM_NAME)
+        assertThat(itemTable.text).contains(ITEM_DESC)
+    }
+
+    @Test
+    fun createInvoiceAddCategory() {
+        val INVOICE_NAME  = "test-invoice-${rng.nextLong()}"
+        val CATEGORY_NAME = "test-category-${rng.nextLong()}"
+
+        open("/index.xhtml")
+
+        s("[id=\"invoiceForm:name\"]").setValue(INVOICE_NAME)
+        s("[id=\"invoiceForm:submitInvoice\"]").click()
+
+        s("[id=\"j_id_j:categoryName\"]").setValue(CATEGORY_NAME)
+        s("[id=\"j_id_j:j_id_m\"]").click()
+
+        // Set category off
+        s("[id=\"j_id_c:j_id_e:0\"]").click()
+        s("[id=\"j_id_c:j_id_h\"]").click()
+
+        // Go to index
+        s("body > a").click()
+
+        var invoiceTable = s("#invoice-table")
+        logger.debug("invoiceTable = ${invoiceTable.text}")
+        assertThat(invoiceTable.text).contains(INVOICE_NAME)
+
+        // Go to OTHERS
+        s("body > a").click()
+
+        var categoryTable =  s("#category-table")
+        logger.debug("catTable = ${categoryTable.text}")
+        assertThat(categoryTable.text).contains(CATEGORY_NAME)
+    }
+
+    @Test
     fun createInvoiceAddItemAddCategory() {
         val INVOICE_NAME  = "test-invoice-${rng.nextLong()}"
         val CATEGORY_NAME = "test-category-${rng.nextLong()}"
